2137906fd742b0159d07ab76b03b722c479b446f
Dal0xE <d.crrenshaw21@gmail.com>
2018-05-15 17:43:24 -0400

    Fix some troublesome type conversions

--- pipspeak.cpp
+++ pipspeak.cpp
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
     f << "RIFF----WAVEfmt ";
     write_word(f, 16, 4); //There is no extension data
     write_word(f, 1, 2); // PCM
-    write_word(f, 2, 2); // Stereo file
+    write_word(f, 1, 2); // Stereo file
     write_word(f, 44100, 4); //Hz (samples/second)
     write_word(f, 176400, 4); // (sample_rate * bitsPerSample * channels) / 8
     write_word(f, 4, 2); // data block size
@@ -63,12 +63,12 @@ int main(int argc, char *argv[]) {
     //audio samples; we need to calculate these from input
     //to do this, we'll use a simple sine wave
 
-    const double two_pi = 6.283185307179586476925286766559;
-    const double max_amplitude = 32760; //volume
+    static double two_pi = 6.283185307179586476925286766559;
+    static double max_amplitude = 32760; //volume
 
     stringstream ss; //We'll need this for extracting args
 
-    double hz = 44100;
+    static double hz = 44100;
     double frequency = 261.626; //initialized to middle-C for no reason
     double seconds; ss << l; ss >> seconds;
 
@@ -77,21 +77,20 @@ int main(int argc, char *argv[]) {
 
     int N = hz * seconds; //total frames
 
-    std::vector<int> subspace;
+    std::vector<double> subspace;
 
     //You're not expected to understand this part, but it helps if you do
     for (int n = 0; n < d.length(); n++) {
         //generate frame subspace
         frequency = 440.0 * pow(1.059463094359, (int) argv[2][n] - 53); // calculate frequency
-        double value = sin((two_pi * frequency) / hz); // This doesn't work. Not sure why. Just doesn't.
-        subspace.push_back((int) (max_amplitude * value));
+        subspace.push_back((two_pi * frequency) / hz);
     }
 
     //Now we need to extrapolate the subspace over the actual sample space
     int a = N / subspace.size();
     for (auto x:subspace) {
         for (int y = 0; y < a; y++) {
-            write_word(f, x, 2); write_word(f, x , 2); //stereo audio
+            write_word(f, (int) (sin(x * y) * 0x7FFF), 2);
         }
     }
     size_t file_length = f.tellp();
